7baf5da22313d8f8646b562ec75d90688ed41455
Kollimarla Rishwanth <kollimarlarishwanth79@gmail.com>
2025-05-24 09:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  n = Number(n); // Ensure n is treated as a number
+  const result = [];
+  let current = [];
+  let sum = 0;
+
+  for (let num of arr) {
+    if (sum + num <= n) {
+      current.push(num);
+      sum += num;
+    } else {
+      if (current.length) {
+        result.push(current);
+      }
+      current = [num];
+      sum = num;
+    }
+  }
+
+  if (current.length) {
+    result.push(current);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
